29a72863a49edbde3ed9eba2dad9fd555c585c85
lmaozeekid <lmaozeekid@hotmail.com>
2025-01-24 19:18:37 +0000

    Initial bot setup

---
+++ bot.js
@@ -0,0 +1,20 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ]
+});
+
+client.once('ready', () => {
+  console.log('Bot is online');
+});
+
+client.on('messageCreate', (message) => {
+  if (message.content === '!ping') {
+    message.channel.send('Pong!');
+  }
+});
+
+client.login('MTMzMjM3ODQ0MjUwNjgyOTkwNA.GqYwGY.rDb8iQfmg4i_TluCOCCgJNJeXfR9pgcXdgGaHU');
